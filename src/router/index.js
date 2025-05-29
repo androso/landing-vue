d0d8e8fb53a3f7c689b6867dce008693f661c705
Zack-y11 <dev.zacknmr28@gmail.com>
2025-05-29 09:21:02 -0600

    refactor: adding file to create routes and using new routes instead of just mount app

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Import views
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Pricing from '../views/Pricing.vue'
+import FAQ from '../views/FAQ.vue'
+import Contact from '../views/Contact.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/pricing',
+    name: 'Pricing',
+    component: Pricing
+  },
+  {
+    path: '/faq',
+    name: 'FAQ',
+    component: FAQ
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+export default router 
