d4d7a6eeb7f07dc887d86f012aace6a4543c2d4e
Zack-y11 <dev.zacknmr28@gmail.com>
2025-05-29 09:23:51 -0600

    refactor: using router view

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,13 @@
 <template>
-    <NavigationSection />
-    <HeroSection />
-    <FeaturesSection/>
-    <HowItWorksSection />
-    <TestimonialsSection />
-    <PricingSection />
-    <DownloadSection />
-    <FAQSection />
-    <NewsletterSection />
-    <Footer />
+    <router-view />
 </template>
 
 <style>
 @import './assets/main.css';
 </style>
-<script>
-    import NavigationSection from "./components/landing/NavigationSection.vue"
-    import HeroSection from "./components/landing/HeroSection.vue"
-    import FeaturesSection from "./components/landing/FeaturesSection.vue"
-    import HowItWorks from "./components/landing/HowItWorks.vue"
-    import TestimonialsSection from "./components/landing/TestimonialsSection.vue"
-    import PricingSection from "./components/landing/PricingSection.vue"
-    import Footer from "./components/landing/Footer.vue"    
-    import DownloadSection from "./components/landing/DownloadSection.vue";
-    import FAQSection from './components/landing/FAQSection.vue'
-import NewsletterSection from './components/landing/NewsletterSection.vue'
 
-    export default {
-        name: 'App',
-        components: {
-            NavigationSection,
-            HeroSection,
-            FeaturesSection,
-            HowItWorksSection: HowItWorks,
-            TestimonialsSection,
-            PricingSection,
-            Footer,
-            DownloadSection,
-            FAQSection,
-            NewsletterSection
-        }
-    }
+<script>
+export default {
+    name: 'App'
+}
 </script>
