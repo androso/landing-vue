24bfa16dfd8e69819d4fe14deb5ec424fac06f1a
Zack-y11 <dev.zacknmr28@gmail.com>
2025-05-29 09:23:11 -0600

    feat: creating new elemnts [views]

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <NavigationSection />
+    <HeroSection />
+    <FeaturesSection/>
+    <HowItWorksSection />
+    <TestimonialsSection />
+    <DownloadSection />
+    <NewsletterSection />
+    <Footer />
+</template>
+
+<script>
+    import NavigationSection from "../components/landing/NavigationSection.vue"
+    import HeroSection from "../components/landing/HeroSection.vue"
+    import FeaturesSection from "../components/landing/FeaturesSection.vue"
+    import HowItWorks from "../components/landing/HowItWorks.vue"
+    import TestimonialsSection from "../components/landing/TestimonialsSection.vue"
+    import Footer from "../components/landing/Footer.vue"    
+    import DownloadSection from "../components/landing/DownloadSection.vue"
+    import NewsletterSection from '../components/landing/NewsletterSection.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            NavigationSection,
+            HeroSection,
+            FeaturesSection,
+            HowItWorksSection: HowItWorks,
+            TestimonialsSection,
+            Footer,
+            DownloadSection,
+            NewsletterSection
+        }
+    }
+</script> 
---
+++ src/views/Pricing.vue
@@ -0,0 +1,28 @@
+<template>
+    <NavigationSection />
+    <div class="py-16">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="text-center mb-12">
+                <h1 class="text-4xl font-bold text-gray-900 mb-4">Pricing Plans</h1>
+                <p class="text-xl text-gray-600">Choose the perfect plan for your needs</p>
+            </div>
+        </div>
+        <PricingSection />
+    </div>
+    <Footer />
+</template>
+
+<script>
+import NavigationSection from "../components/landing/NavigationSection.vue"
+import PricingSection from "../components/landing/PricingSection.vue"
+import Footer from "../components/landing/Footer.vue"
+
+export default {
+    name: 'Pricing',
+    components: {
+        NavigationSection,
+        PricingSection,
+        Footer
+    }
+}
+</script> 
